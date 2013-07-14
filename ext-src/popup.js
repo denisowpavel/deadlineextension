4c6ea2272e6531f1789c1fff4384e264cba20a51
Denisow Pavel <itlab@list.ru>
2013-07-14 19:11:52 +0700

    set up progress picture

--- ext-src/popup.js
+++ ext-src/popup.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-
+	var val = 130;	
+	if(val < 1){
+		val = 1;
+	}else if(val > 100){
+		val = 100;
+	}
+	if(val < 10){
+		sVal = "00"+val;
+	}else if(val < 100){
+		sVal = "0"+val;
+	}else{
+		sVal = val;
+	}
+	
+	$("img#progress").attr("src","img/progress0"+sVal+".png");
 });
