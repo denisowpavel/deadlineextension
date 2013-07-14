10d09e8837a22205fa2746692624e973c2231d45
Denisow Pavel <itlab@list.ru>
2013-07-14 19:32:02 +0700

    add bg and text color

--- ext-src/popup.js
+++ ext-src/popup.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var val = 130;	
+	var val = 80;	
 	if(val < 1){
 		val = 1;
 	}else if(val > 100){
@@ -12,6 +12,12 @@ $(document).ready(function() {
 	}else{
 		sVal = val;
 	}
-	
+	var bgColor = "#FFF";
+	var textColor = "#000";
+	if(val >= 85){
+		bgColor = "#000";
+		textColor = "#FFF";
+	}
+	$("body").css("background-color", bgColor);
 	$("img#progress").attr("src","img/progress0"+sVal+".png");
 });
