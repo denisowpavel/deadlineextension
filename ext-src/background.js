bc5737a4d67a82ad3bb674803ecd3234fb5bdeb9
Denisow Pavel <itlab@list.ru>
2013-08-15 13:56:27 +0700

    remove debug and set timer to 11 min

--- ext-src/background.js
+++ ext-src/background.js
@@ -1,14 +1,8 @@
-// var iTemp = 0
-
-// function updateBadge(){	
-//     //chrome.browserAction.setBadgeText ( { text: iTemp+"" } );
-//     iTemp++;
-// }
 
 chrome.alarms.clearAll()
 loadDatesFromStorage(false);
 
-chrome.alarms.create("main",{periodInMinutes: 0.1});
+chrome.alarms.create("main",{periodInMinutes: 11});
 chrome.alarms.onAlarm.addListener(function(alarm) {
 	loadDatesFromStorage(false);	
 });
